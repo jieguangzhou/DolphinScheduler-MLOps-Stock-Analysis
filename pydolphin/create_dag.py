ef06d74d47ed53d42aeb4bb5db9013ea3afe89b7
jieguangzhou <jieguang_zhou@163.com>
2022-07-23 23:06:14 +0800

    support read script from file

--- pydolphin/create_dag.py
+++ pydolphin/create_dag.py
@@ -1,7 +1,10 @@
-from pydolphinscheduler.core.process_definition import ProcessDefinition
-from pydolphinscheduler import tasks
-from ruamel import yaml
+import os
+import re
+
 import click
+from pydolphinscheduler import tasks
+from pydolphinscheduler.core.process_definition import ProcessDefinition
+from ruamel import yaml
 
 
 def load_yaml(yaml_file) -> dict:
@@ -15,13 +18,14 @@ def load_yaml(yaml_file) -> dict:
 def create_process_definition(yaml_file):
     config = load_yaml(yaml_file)
     process_params = config["Process"]
-    execute = process_params.pop('execute', 'submit')
+    execute = process_params.pop("execute", "submit")
     with ProcessDefinition(**process_params) as pd:
         dependencies = {}
         name2task = {}
         for task_data in config["Tasks"]:
             task_type = task_data["TaskType"]
             task_params = task_data["params"]
+            parse_params(task_params)
 
             task_cls = getattr(tasks, task_type)
             task = task_cls(**task_params)
@@ -36,13 +40,27 @@ def create_process_definition(yaml_file):
             for upstram_task_name in deps:
                 upstram_task = name2task[upstram_task_name]
                 upstram_task >> downstream_task
-        
-        if execute == 'submit':
+
+        if execute == "submit":
             pd.submit()
-        elif execute == 'run':
+        elif execute == "run":
             pd.run()
         else:
             assert False
+
+
+def parse_params(params):
+    for key, value in params.items():
+        if isinstance(value, str):
+            params[key] = parse_string_param_if_file(value)
+
+
+def parse_string_param_if_file(string_param: str):
+    if string_param.startswith("File"):
+        path = re.findall(r"File\(\"(.*?)\"\)", string_param)[0]
+        with open(path, "r") as read_file:
+            string_param = "".join(read_file)
+    return string_param
 
 
 if __name__ == "__main__":
